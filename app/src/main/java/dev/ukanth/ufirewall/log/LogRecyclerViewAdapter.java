0cf92ef789fbc9873aa73b89294c5e9f76615f31
amaa-99 <->
2023-07-29 23:35:32 +0200

    * [Issue #1] Application crashes (immediately) after choosing ' View log' from the menu. Addressed a couple of lint warnings.

--- app/src/main/java/dev/ukanth/ufirewall/log/LogRecyclerViewAdapter.java
+++ app/src/main/java/dev/ukanth/ufirewall/log/LogRecyclerViewAdapter.java
@@ -3,7 +3,6 @@ package dev.ukanth.ufirewall.log;
 import static dev.ukanth.ufirewall.Api.TAG;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
@@ -12,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.DateFormat;
@@ -29,10 +29,7 @@ public class LogRecyclerViewAdapter extends RecyclerView.Adapter<LogRecyclerView
 
     private final List<LogData> logData;
     private final Context context;
-    private LogData data;
-    private PackageInfo info;
     private final RecyclerItemClickListener recyclerItemClickListener;
-    private View mView;
 
     public LogRecyclerViewAdapter(final Context context, RecyclerItemClickListener recyclerItemClickListener) {
         this.context = context;
@@ -45,9 +42,10 @@ public class LogRecyclerViewAdapter extends RecyclerView.Adapter<LogRecyclerView
         logData.addAll(logDataList);
     }
 
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.log_recycle_item, parent, false);
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.log_recycle_item, parent, false);
         return new ViewHolder(mView);
     }
 
@@ -97,9 +95,9 @@ public class LogRecyclerViewAdapter extends RecyclerView.Adapter<LogRecyclerView
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        data = logData.get(position);
+        LogData data = logData.get(position);
         PackageManager manager = context.getPackageManager();
-        holder.bind(logData.get(position),recyclerItemClickListener);
+        holder.bind(logData.get(position), recyclerItemClickListener);
         try {
             Drawable applicationIcon = Api.getApplicationIcon(context, data.getUid());
             holder.icon.setBackground(applicationIcon);
